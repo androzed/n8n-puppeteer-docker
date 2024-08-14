072314be1e6accb90a7ab22105cc374f8d49377b
androzed <88204980+androzed@users.noreply.github.com>
2024-08-14 17:54:22 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source ./env.sh
-docker build --build-arg N8N_VERSION=$N8N_VERSION -t n8n-puppeteer:$N8N_VERSION .
+docker build --build-arg N8N_VERSION=1.53.2 -t n8n-puppeteer:1.53.2 .
